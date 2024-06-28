d6298f980868aeb17d40d781a4e7ce8d5cc1e4e8
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-06-28 22:19:18 +0530

    send-email api created

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors')
 const path = require('path')
+const nodemailer = require('nodemailer')
 
 dotenv.config();
 
@@ -27,6 +28,41 @@ else {
 }
 
 // -------------Deployment----------------
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false, // upgrade later with STARTTLS
+    auth: {
+        user: process.env.PROTON_USERNAME,
+        pass: process.env.PROTON_PASSWORD,
+    }
+
+});
+
+app.post("/send-email", async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+        const mailOptions1 = {
+            from: process.env.PROTON_USERNAME,
+            to: process.env.ADMIN_EMAIL,
+            subject: 'PORTFOLIO API TEST',
+            html: `<strong>${name} wants to get in touch with you! it works!</strong> <br> <p> ${message} </p> <br> <h4>Contact him at: ${email} </h4>`
+        };
+
+        const data = await transporter.sendMail(mailOptions1);
+        const mailOptions2 = {
+            from: process.env.PROTON_USERNAME,
+            to: email,
+            subject: `Thanks! Utkarsh Paliwal will contact you soon.`,
+            html: `<h1> Hey ${name}! Thanks for visiting my website. </h1> <br> <h3> I will contact you soon. </h3>`
+        }
+
+        const data2 = await transporter.sendMail(mailOptions2);
+        res.status(200).json({ "1": data, "2": data2 });
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
 
 
 const PORT = process.env.PORT;
