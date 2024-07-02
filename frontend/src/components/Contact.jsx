aa75cc23978ef0e02de8c75b4d27178b592a7816
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-07-02 13:28:04 +0530

    Loader and Toaster added

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import axios from 'axios';
 import { baseURL } from "../../baseURL";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Contact() {
     const [formData, setFormData] = useState({
@@ -8,6 +11,8 @@ function Contact() {
         email: '',
         message: ''
     });
+
+    const [isLoading, setIsLoading] = useState(false);
 
 
     const handleChange = (e) => {
@@ -18,45 +23,68 @@ function Contact() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(formData);
+        setIsLoading(true);
         try {
             const { data } = await axios.post(`${baseURL}/send-email`, formData);
             if (!data) {
                 console.log('something went wrong');
+                toast.error('some error occured while sending mail')
             }
             else {
                 console.log('mail sent successfully');
+                toast.success('mail sent successfully')
+                setFormData({
+                    name: '',
+                    email: '',
+                    message: ''
+                })
             }
+            setIsLoading(false);
         } catch (error) {
+            setIsLoading(false);
+            toast.error('some error occured while sending mail')
             console.log(error);
         }
     }
     return (
-        <div id="contact" className="border h-[500px] flex justify-center w-full bg-white py-10 px-2 sm:px-10 md:px-32">
-            <div className=" w-full max-w-[650px] p-2 sm:p-4 rounded-md">
-                <div>
-                    <h1 className="text-4xl">Get in touch</h1>
+        <>
+            <div id="contact" className="border h-[500px] flex justify-center w-full bg-white py-10 px-2 sm:px-10 md:px-32">
+                <div className=" w-full max-w-[650px] p-2 sm:p-4 rounded-md">
+                    <div>
+                        <h1 className="text-4xl">Get in touch</h1>
+                    </div>
+                    <form className="text-xl" onSubmit={handleSubmit}>
+                        <div className="flex flex-col">
+                            <label htmlFor="name">Your Name</label>
+                            <input onChange={handleChange} required value={formData.name} className="p-1 focus:outline-1 focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="name" id="name" placeholder="Utkarsh Paliwal" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <label htmlFor="email">Your Email</label>
+                            <input onChange={handleChange} required value={formData.email} className="p-1 focus:outline-1 focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="email" id="email" placeholder="utkarsh@example.com" />
+                        </div>
+                        <div className="flex flex-col mt-2">
+                            <label htmlFor="message">Your Message</label>
+                            <textarea onChange={handleChange} value={formData.message} className="p-1 focus:outline-1 min-h-[120px] max-h-[120px] focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="message" id="message" placeholder="Type your message here..." ></textarea>
+                        </div>
+                        <div className="mt-2 font-sans">
+                            <button type="submit" className="text-black hover:text-white border border-black hover:bg-black focus:ring-4 focus:ring-black font-medium rounded-md text-sm px-5 py-2.5 text-center me-2 inline-flex items-center hover:transition transition duration-700" disabled={isLoading}>
+                                {isLoading ?
+                                    <div>
+                                        <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-black hover:text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                                        </svg>
+                                        Loading...
+                                    </div> :
+                                    <h1 className="px-8 text-md">Send</h1>
+                                }
+                            </button>
+                        </div>
+                    </form>
                 </div>
-                <form className="text-xl" onSubmit={handleSubmit}>
-                    <div className="flex flex-col">
-                        <label htmlFor="name">Your Name</label>
-                        <input onChange={handleChange} value={formData.name} className="p-1 focus:outline-1 focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="name" id="name" placeholder="Utkarsh Paliwal" />
-                    </div>
-                    <div className="flex flex-col mt-2">
-                        <label htmlFor="email">Your Email</label>
-                        <input onChange={handleChange} value={formData.email} className="p-1 focus:outline-1 focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="email" id="email" placeholder="utkarsh@example.com" />
-                    </div>
-                    <div className="flex flex-col mt-2">
-                        <label htmlFor="message">Your Message</label>
-                        <textarea onChange={handleChange} value={formData.message} className="p-1 focus:outline-1 min-h-[120px] max-h-[120px] focus:outline-black ring-black border border-black rounded font-light font-sans text-md" type="text" name="message" id="message" placeholder="Type your message here..." ></textarea>
-                    </div>
-                    <div className="mt-2">
-                        <button type="submit" className="w-32 h-10 border-black border hover:bg-black hover:text-white rounded">
-                            <h1 className="text-xl font-sans hover:transition transition duration-700">Send</h1>
-                        </button>
-                    </div>
-                </form>
             </div>
-        </div>
+            <ToastContainer />
+        </>
     )
 }
 
